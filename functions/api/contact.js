d206d41f9b61f7b75843344b0cd5270d711326f8
Yandi <yandirb2012@gmail.com>
2026-07-28 13:50:19 -0700

    Cloudflare verification with key

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -8,14 +8,57 @@ export async function onRequest(context) {
 
     const formData = await context.request.formData();
 
-    const data = {
-        name: formData.get("name"),
-        company: formData.get("company"),
-        email: formData.get("email"),
-        project: formData.get("project"),
-        message: formData.get("message"),
-        turnstile: formData.get("cf-turnstile-response")
-    };
+    const token = formData.get("cf-turnstile-response");
 
-    return Response.json(data);
+    if (!token) {
+        return Response.json(
+            {
+                success: false,
+                error: "Missing Turnstile token."
+            },
+            {
+                status: 400
+            }
+        );
+    }
+
+    const ip =
+        context.request.headers.get("CF-Connecting-IP");
+
+    const verify = await fetch(
+        "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type":
+                    "application/x-www-form-urlencoded"
+            },
+            body: new URLSearchParams({
+                secret: context.env.TURNSTILE_SECRET,
+                response: token,
+                remoteip: ip ?? ""
+            })
+        }
+    );
+
+    const result = await verify.json();
+
+    if (!result.success) {
+
+        return Response.json(
+            {
+                success: false,
+                turnstile: result
+            },
+            {
+                status: 403
+            }
+        );
+    }
+
+    return Response.json({
+        success: true,
+        message: "Turnstile validated successfully."
+    });
+
 }
